d6479c3b17624b10f81fb31bd0be81712790ffa4
TosinAji <tosinajimobi@gmail.com>
2012-08-20 17:53:15 +0530

    TA and AS: created showprofile.jsp

--- src/main/java/com/forum/web/controller/UserController.java
+++ src/main/java/com/forum/web/controller/UserController.java
@@ -3,15 +3,29 @@ package com.forum.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class UserController {
 
 
     @RequestMapping("/join")
-    public ModelAndView activityView() {
+    public ModelAndView registerView() {
         return new ModelAndView("join");
     }
+
+//    @RequestMapping(value = "/showprofile", method = RequestMethod.POST)
+//    public ModelAndView showProfile(@RequestParam Map<String, String> params){
+//
+//        ModelAndView modelAndView = new ModelAndView("showprofile");
+//        modelAndView.addObject("username",params.get("username"));
+//        modelAndView.addObject("name",params.get("name"));
+//        return modelAndView;
+//
+//    }
 
 }
---
+++ src/main/webapp/WEB-INF/view/showprofile.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Forum - Your Profile</title>
+</head>
+<body class="userProfile">
+<div class="title">
+    <h2>You have successfully submitted your user details.</h2>
+    <div><label class="formLabels">Username:</label>${username}</div>
+    <div>
+        <label class="formLabels"> Name: </label>
+        ${name}
+    </div>
+</div>
+</body>
+</html>
