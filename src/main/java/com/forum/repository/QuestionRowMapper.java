35274450683ade991e753a57c0b75f86740bf5c8
Kunal Powar <kunalup@thoughtworks.com>
2012-08-24 16:21:25 +0530

    Kunal: last few changes

--- src/main/java/com/forum/repository/QuestionRowMapper.java
+++ src/main/java/com/forum/repository/QuestionRowMapper.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public class QuestionRowMapper implements RowMapper {
 
-//    @Override
+   @Override
     public Object mapRow(ResultSet resultSet, int i) throws SQLException {
         Question question = new Question(resultSet.getInt("ID"), resultSet.getString("TITLE"),resultSet.getString("DESCRIPTION"),resultSet.getInt("USER_ID"),resultSet.getString("CREATED_AT"));
         return question;
