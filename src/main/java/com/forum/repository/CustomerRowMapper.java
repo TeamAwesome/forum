0f6b1ed4702c9e117690b09cc4ac2de897eb9942
Kunal Powar <kunalup@thoughtworks.com>
2012-08-24 14:36:22 +0530

    KUNAL: commit before pull

--- src/main/java/com/forum/repository/CustomerRowMapper.java
+++ src/main/java/com/forum/repository/CustomerRowMapper.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerRowMapper implements RowMapper {
-    @Override
+//    @Override
     public Customer mapRow(ResultSet resultSet, int row) throws SQLException {
         return new Customer(resultSet.getInt("ID"), resultSet.getString("FIRST_NAME"), resultSet.getString("LAST_NAME"));
     }
--- src/main/java/com/forum/web/controller/AdminController.java
+++ src/main/java/com/forum/web/controller/AdminController.java
@@ -24,4 +24,6 @@ public class AdminController {
         modelAndView.addObject("month",params.get("month.value"));
         return modelAndView;
     }
+
+
 }
