34fde3fd42ac68396b97a80e9b1b68e0a7c545a5
TosinAji <tajimobi@thoughtworks.com>
2012-08-31 01:00:00 +0530

    [Tosin] - Finished changing the test data.

--- scripts/sql/testdata/integration_test_data.sql
+++ scripts/sql/testdata/integration_test_data.sql
@@ -52,137 +52,140 @@ INSERT INTO QUESTION(
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:01:04', 1);
+  VALUES('Can anyone recommend a good map for exploring Bangalore with?', 'Please?', '2012-08-23 10:01:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:02:04', 1);
+  VALUES('Guys, I don\'t believe it', 'There was totally a cow in the road today, it caused such a massive traffic jam I couldn\'t even believe it!', '2012-08-23 10:02:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:03:04', 1);
+  VALUES('Why is it so cold?', 'I know it\'s monsoon season and everything, but I\'m like totally freezing. You know? Gosh, like, hand me a sweater already! Can I even buy a sweater in Bangalore, like, I dunno.' ,
+            '2012-08-23 10:03:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:04:04', 1);
+  VALUES('Thinking of moving to Bangalore', 'Hi, I\'m currently living in the U.S. but My company recently offered me the opportunity to work in Bangalore for at least a year. I\'m seriously considering it, but I\'d like to get some advice from people who have possibly already been in a similar situation. Can anybody help?' ,
+            '2012-08-23 10:04:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:05:04', 1);
+  VALUES('What\'s up guys', 'How\'s life treating everyone?', '2012-08-23 10:05:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:06:04', 1);
+  VALUES('Does anyone know...', '...if the ice is made from drinking water here?', '2012-08-23 10:06:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:07:04', 1);
+  VALUES('Places to stay', 'Has anyone stayed in the Diamond District, is it a nice place? I\'m in Bangalore for 2 months so I want to be in a nice place.', '2012-08-23 10:07:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:08:04', 1);
+  VALUES('Which is more popular, Cricket or Hockey?', 'See Title', '2012-08-23 10:08:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:09:04', 1);
+  VALUES('Woo I\'m so excited to be here!' , 'What should I go see first! :D', '2012-08-23 10:09:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:10:04', 1);
+  VALUES('I can\'t decide where to go', 'I\'ve heard commercial street is really great for shopping, but I\'ve heard about Brigade road too. Which one should I go to first?' ,
+            '2012-08-23 10:10:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:11:04', 1);
+  VALUES('What kind of music is popular in Bangalore', 'I really love going to music shows, and I\'m not picky about the kind of music I listen too. Can anyone recommend some acts I could see in the next month or so?', '2012-08-23 10:11:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:12:04', 1);
+  VALUES('Why are there signs all over Koramangala that say B-trac?', 'I want to know!', '2012-08-23 10:12:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:13:04', 1);
+  VALUES('Why does spicy ice cream exist?', 'why?', '2012-08-23 10:13:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:14:04', 1);
+  VALUES('I\'m doing a pecha kucha on Bangalore for school'  , 'Can everyone tell me things about Bangalore?', '2012-08-23 10:14:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:15:04', 1);
+  VALUES('Is Bangalore worth visiting', 'I want to either visit Delhi or Bangalore, I only have a week to spend, so I can\'t visit both, but if you guys tell me it\'s good then I might postpone my trip until I can spend longer out there.' , '2012-08-23 10:15:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-23 10:16:04', 1);
+  VALUES("What's the local language in Bangalore", "I can't remember. D:", '2012-08-23 10:16:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-13 10:18:04', 1);
+  VALUES("How long to Pondicherry", "I live in Bangalore, and I want to explore India a bit, how long will it take me to drive from Bangalore to Pondicherry.", '2012-08-13 10:18:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-13 10:19:04', 1);
+  VALUES("What good Hindi movies are out?", "Also where's the nicest cinema in Bangalore to go an see them in?", '2012-08-13 10:19:04', 1);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-25 10:19:54', 3);
+  VALUES('Crossing the road is scary', "I don't understand! How do people cross roads here? D: ", '2012-08-25 10:19:54', 3);
 
 INSERT INTO QUESTION(
     TITLE,
     DESCRIPTION,
     CREATED_AT,
     USER_ID)
-  VALUES('What\'s up today Lu', 'I don\'t know. You tell me Du Juan', '2012-08-13 10:20:04', 2);
+  VALUES("What's the weather like in Bangalore currently? ", "I want to visit, but I hear it's been raining", '2012-08-13 10:20:04', 2);
