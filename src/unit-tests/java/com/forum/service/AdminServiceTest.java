db4609f56daabbb324286af2144207ce6fc7e31e
TosinAji <tajimobi@thoughtworks.com>
2012-08-30 11:49:37 +0530

    Trishna/Tosin - Fixed the build by removing test that checked if records were counted for each month i.e. no longer a requirement

--- src/unit-tests/java/com/forum/service/AdminServiceTest.java
+++ src/unit-tests/java/com/forum/service/AdminServiceTest.java
@@ -34,48 +34,4 @@ public class AdminServiceTest {
         questions.add(new Question(3, "test","test",user,new Date()));
         assertThat(adminService.getTotalNumberOfQuestions(), is(questions.size()));
     }
-
-    @Test
-    public void shouldGenerateCountOfRecordsForEachMonth() {
-        List<Question> questions = new ArrayList<Question>();
-
-        User user = new User("Tom", "pass", "Tom Tom", "tom@tom.com", "1234567",
-                "Moon", "He doesn't know", 200);
-
-        questions.add(new Question(01,"mock question","mock description",user, addMonth(-1)));
-        questions.add(new Question(02,"mock question","mock description",user, addMonth(-2)));
-        questions.add(new Question(03,"mock question","mock description",user, addMonth(-5)));
-        questions.add(new Question(04,"mock question","mock description",user, addMonth(-7)));
-        questions.add(new Question(05,"mock question","mock description",user, addMonth(-7)));
-        questions.add(new Question(06,"mock question","mock description",user, addMonth(-7)));
-
-        QuestionRepository questionRepository = mock(QuestionRepository.class);
-        when(questionRepository.getQuestionsPostedInLast12Months()).thenReturn(questions);
-
-        AdminService adminService = new AdminService(questionRepository);
-        List<Integer> count = new ArrayList<Integer>();
-        count = adminService.getQuestionsPostedInLast12Months();
-
-        assertThat("0 months ago", count.get(0), is(0));
-        assertThat("1 months ago", count.get(1), is(1));
-        assertThat("2 months ago", count.get(2), is(1));
-        assertThat("3 months ago", count.get(3), is(0));
-        assertThat("4 months ago", count.get(4), is(0));
-        assertThat("5 months ago", count.get(5), is(1));
-        assertThat("6 months ago", count.get(6), is(0));
-        assertThat("7 months ago", count.get(7), is(3));
-        assertThat("8 months ago", count.get(8), is(0));
-        assertThat("9 months ago", count.get(9), is(0));
-        assertThat("10 months ago", count.get(10), is(0));
-        assertThat("11 months ago", count.get(11), is(0));
-    }
-
-    private Date addMonth(int month) {
-        GregorianCalendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.MONTH, month);
-
-        Date result = calendar.getTime();
-        logger.info(result.toString());
-        return result;
-    }
 }
