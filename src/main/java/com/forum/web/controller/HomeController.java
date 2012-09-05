11c1131d779667a10d12ab9e38de6e97980c26ab
Arun Sundaralingam <arunms@thoughtworks.com>
2012-09-05 10:44:05 +0530

    [Arun|Rohit] Tag cloud problems resolved

--- src/main/java/com/forum/web/controller/HomeController.java
+++ src/main/java/com/forum/web/controller/HomeController.java
@@ -5,10 +5,12 @@ import com.forum.service.QuestionService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
